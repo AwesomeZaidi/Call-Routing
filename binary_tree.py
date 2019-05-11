a56c3ddc33b689f56d6601745e159455b3e40ce5
Asim Zaidi <asimzaidih@gmail.com>
2019-05-11 14:17:56 -0700

    starting binary tree

---
+++ binary_tree.py
@@ -0,0 +1,10 @@
+# !python
+
+
+class BinaryTreeNode(object):
+
+    def __init__(self, data):
+        """Initialize this binary tree node with the given data."""
+        self.data = data
+        self.left = None
+        self.right = None
