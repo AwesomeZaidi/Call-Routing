b1655e9ea172013008eebf895ce59cd80198e019
Asim Zaidi <asimzaidih@gmail.com>
2019-05-11 14:24:52 -0700

    [added] node height

--- binary_tree.py
+++ binary_tree.py
@@ -21,4 +21,25 @@ class BinaryTreeNode(object):
         """Return True if this node is a branch (has at least one child)."""
         return self.left is not None or self.right is not None
     
+
+    def height(self):
+        """Return the height of this node (the number of edges on the longest
+        downward path from this node to a descendant leaf node).
+        TODO: Best and worst case running time: ??? under what conditions?"""
+
+        left_height = 0
+        right_height = 0
+
+        # if left child has a value, calculate its height
+        if self.left:
+            left_height = 1 +self.left.height()
+        
+        # if right child has a value, calculate its height
+        if self.right:
+            right_height = 1 +self.right.height()
     
+        # Return the greater of the left height and right height + 1
+        return 1 + max(left_height, right_height)
+
+    
+
