6f2e373466a0f8a5b32c23daaeaa9a94673106ba
Asim Zaidi <asimzaidih@gmail.com>
2019-05-11 14:23:32 -0700

    [added] is_leaf, is_branch, height function

--- binary_tree.py
+++ binary_tree.py
@@ -8,3 +8,17 @@ class BinaryTreeNode(object):
         self.data = data
         self.left = None
         self.right = None
+    
+    def __repr__(self):
+        """Return a string representation of this binary tree node."""
+        return 'BinaryTreeNode({!r})'.format(self.data)
+
+    def is_leaf(self):
+        """Return True if this node is a leaf (has no children)."""
+        return self.left is None and self.right is None
+
+    def is_branch(self):
+        """Return True if this node is a branch (has at least one child)."""
+        return self.left is not None or self.right is not None
+    
+    
