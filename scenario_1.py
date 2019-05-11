d1aeddadcaf8358fd5449832eaaa068e973ce547
Asim Zaidi <asimzaidih@gmail.com>
2019-05-11 16:02:49 -0700

    scenario one almost working 🚧

--- scenario_1.py
+++ scenario_1.py
@@ -29,17 +29,43 @@ def read_file(file_name):
 	"""
 		Opens the file and splits the numbers into a list.
 	"""
-	file = open(file_name, "r")
-	number_data = file.readline()
-	file.close()
-	number_data = re.split(',|\n', number_data)
+	with open('data/' + file_name, "r") as file:
+		number_data = file.read()
+		number_data = re.split(',|\n', number_data)
 	return number_data
 
-#  find the longest prefix match
+def find_route_cost(number_data, phone_number):
+	"""
+		number_data: list of number, cost, number, cost, etc.
+		Finds the longest route match and returns the associated cost.
+		Runtime: O(n)- O(p*n)
+		p = len of phone_number
+		n = len of number_data list
+	"""
+	print('phone number:', phone_number)
+	# for every other element in number_data:
+	for index, element in enumerate(number_data[::2]):
+		print('element:', element)
+		if phone_number in number_data:
+			print('found number in number_data')
+			return number_data[index+2]
+		else:
+			phone_number = phone_number[:index-1]
+			print('new phone_number:', phone_number)
+	# if number not found
+	return 0
 
+
+
+  # if phone_number not in number_data:
+	# 	index -= 1
+	# 	phone_number = phone_number[:index]
 # write to a new text file with the answer
 
 if __name__ == "__main__":
-  #  scenario playing like how Edwin had it.
-	pass
+	#  scenario playing like how Edwin had it.
+	paths = read_file("route-costs-4.txt")
+	# print(paths)
+	number_route_cost = find_route_cost(paths, '+14152348')
+	print(number_route_cost)
 	
