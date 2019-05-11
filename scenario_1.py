c8a8dfaff0203a83216e1eb415a89e7032f1a2cb
Asim Zaidi <asimzaidih@gmail.com>
2019-05-11 14:18:14 -0700

    [updated] scenario 1 top level comment

--- scenario_1.py
+++ scenario_1.py
@@ -1,4 +1,14 @@
-#  SCENARIO 1
-# One-time route cost check:
-# You have a carrier route list with 100,000 (100k) entries (in arbitrary order) and a single phone number.
-# How quickly can you find the cost of calling this number?
+# ==================================================================================
+# File: solution_1.py
+#
+# One-time route cost check
+# Desc: You have a carrier route list with 100,000 (100K) entries (in arbitrary order)
+# and a single phone number. How quickly can you find the cost of calling this number?
+
+# Copyright © 2019 Jaycee Azua and Asim Zaidi. All rights reserved.
+# ==================================================================================
+
+# ----------------------------------------------------------------------------------
+# Imports
+# ----------------------------------------------------------------------------------
+
