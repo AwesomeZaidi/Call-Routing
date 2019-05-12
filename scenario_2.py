b38d308917f1c4dc9a1a2467a3a0f5cb5e752a9b
Jayce Azua <jayce.azua@gmail.com>
2019-05-11 21:05:12 -0700

    [done] scenario 2

--- scenario_2.py
+++ scenario_2.py
@@ -36,40 +36,48 @@ class CallRouter(object):
         hash_lookup = HashTable()
         with open('data/' + path_to_file, "r") as file:
            for line in file:
-            carrier, cost = line.split(",")
-            print('hash_lookup cost:', cost)
-            if hash_lookup.contains(carrier):
-                original_cost = hash_lookup.get(carrier)
-                if cost < original_cost:
+                line = line[:-1]
+                carrier, cost = line.split(",")
+                
+                if hash_lookup.contains(carrier):
+                    original_cost = hash_lookup.get(carrier)
+
+                    if cost < original_cost:
+                        hash_lookup.set(carrier, cost)
+                else:
                     hash_lookup.set(carrier, cost)
-            else:
-                hash_lookup.set(carrier, cost)
         return hash_lookup
 
     def read_number(self, path_to_file):
         with open('data/' + path_to_file, "r") as file:
            for line in file:
-               self.find_route_cost(line)
+               line = line[:-1]
+               cost = self.find_route_cost(line)
+               self.write_cost(line, cost)
 
     def find_route_cost(self, phone_number):
+        print("phone:", phone_number)
         for _ in phone_number:
+
             if self.route_costs.contains(phone_number):
+
                 cost = self.route_costs.get(phone_number)
-                print("Cost:", cost)
-                self.__write_cost(phone_number, cost)
-                return
+                return cost
+
             else:
                 phone_number = phone_number[:len(phone_number)-1]
+        return str(0)
+        
 
-    def __write_cost(self, phone_number, cost):
+    def write_cost(self, phone_number, cost):
         with open("route-costs-2.txt", "a") as f:
-            f.write(phone_number + ", " + cost)
+            f.write(phone_number + ", " + cost + "\n")
 
 def test_call_router():
     # phone_numbers_path = 'data/phone-numbers-1000.txt'
     carrier_route_path = 'carrier-routes-4.txt'
     call_router = CallRouter(carrier_route_path)
-    call_router.read_number('phone-numbers-1000.txt')
+    call_router.read_number('phone-numbers-3.txt')
     return call_router.route_costs.values()
     # return
 
