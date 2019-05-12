d84e70347ec07cffd17cc3445a42f1a443a719d9
Asim Zaidi <asimzaidih@gmail.com>
2019-05-11 21:06:28 -0700

    added scenario 3 file comment

--- scenario_3.py
+++ scenario_3.py
@@ -1,5 +1,11 @@
-#  SCENARIO 3
-# Multiple long carrier route lists:
+# !python
+
+# ==================================================================================
+# File: scenario_3.py
+
 # You have 5 carrier route lists, each with 10,000,000 (10m) entries (in arbitrary order)
 # and a list of 10,000 phone numbers. How can you speed up your route cost lookup
 # solution to handle this larger dataset
+
+# Copyright © 2019 Jayce Azua and Asim Zaidi. All rights reserved.
+# ==================================================================================
