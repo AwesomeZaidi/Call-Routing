f48eab9e8b0b11ac521de8d98ebb06f5c24bc89b
Jayce Azua <jayce.azua@gmail.com>
2019-05-12 14:34:46 -0700

    [add] sorting method

--- scenario_3.py
+++ scenario_3.py
@@ -10,6 +10,63 @@
 # Copyright © 2019 Jayce Azua and Asim Zaidi. All rights reserved.
 # ==================================================================================
 
+import re
+import os
+import glob
+
+
+class TrieNode:
+  def __init__(self, value=None):
+      self.value = value
+      self.dictionary = {}
+      self.cost = None
+
+class Trie:
+  def __init__(self, files):
+    self.root = TrieNode()
+    for file in files:
+      self.buildTrieOfRoutes(file)
+
+  def buildTrieOfRoutes(self, file):
+    """
+      Takes in a file with lines of 'route,costs' and builds a Trie of the routes' digits.
+    """
+    for line in open(file):
+      current = self.root
+      route, cost = line.split(",")
+      cost = float(cost.strip("\n"))
+      for digit in route:
+        if digit not in current.dictionary:
+            current.dictionary[digit] = TrieNode(digit)
+        current = current.dictionary[digit]
+      if current.cost:
+        if current.cost > cost:
+            current.cost = cost
+      else:
+        current.cost = cost
+
+  def findLowestCostsAndPrintThemToFile(self, file):
+    """Finds the lowest costs for each phone number in the input file."""
+
+    for phoneNumber in open(file):
+      current = self.root
+      minimum = float('inf')
+      for digit in phoneNumber:
+        if digit in current.dictionary:
+          if current.cost:
+              minimum = min(minimum, current.cost)
+          current = current.dictionary[digit]
+        else:
+          phoneNumber = phoneNumber.strip("\n")
+          if minimum != float('inf'):
+              with open("output_logs/route-costs-3.txt", "a+") as f:
+                      f.write(phoneNumber + ", " + str(minimum) + '\n')
+          else:
+              with open("output_logs/route-costs-3.txt", "a+") as f:
+                      f.write(phoneNumber + ", 0 \n")
+          break  # break out of the 'for digit in phoneNumber' loop
+          # and start on the next phoneNumber.
+
 # ideas: sorting our data:
 class Quick_Sort(object):
   def __init__(self, arbitrary_order):
@@ -46,3 +103,7 @@ class Quick_Sort(object):
         arbitrary_order[i], arbitrary_order[border] = arbitrary_order[border], arbitrary_order[i]
     arbitrary_order[low], arbitrary_order[border] = arbitrary_order[border], arbitrary_order[low]
     return border
+
+
+if __name__ == "__main__":
+  pass
