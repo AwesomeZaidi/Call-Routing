abc088a6a9c4648948b0826317de308ce28a9ce8
Asim Zaidi <asimzaidih@gmail.com>
2019-05-12 18:25:20 -0700

    [Finished] building trie tree

--- scenario_3.py
+++ scenario_3.py
@@ -13,97 +13,80 @@
 import re
 import os
 import glob
+from trie import Trie
+
+def build_trie(file_path: str) -> Trie:
+    """ Returns a Trie tree from the given file_path. Costs of routes."""
+    # get number prefixes and costs from 
+    # lines = numbers_and_costs_from(data_file_path)
+    trie = Trie()
+
+    # iterates through routes and costs
+    with open(file_path, 'r') as f:
+        lines = (l.split(',') for l in f.readlines())
+
+    for route, cost in lines:
+        trie.insert(route[1:], float(cost))
+
+    return trie
+
+def test_call_router():
+      route_costs_path = 'data/route-costs-10000000.txt'
+      route_costs_trie = build_trie(route_costs_path)
+
+      numbers = (number for number in
+        open('data/phone-numbers-1000.txt').readlines())
+      
+      with open('output/call-costs-3.txt', 'w') as output_file:
+        for number in numbers:
+            cost = route_costs_trie.search(number)
+            output_file.write(f'{number},{cost}\n')
+
+if __name__ == '__main__':
+  test_call_router()
 
 
-class TrieNode:
-  def __init__(self, value=None):
-      self.value = value
-      self.dictionary = {}
-      self.cost = None
-
-class Trie:
-  def __init__(self, files):
-    self.root = TrieNode()
-    for file in files:
-      self.buildTrieOfRoutes(file)
-
-  def buildTrieOfRoutes(self, file):
-    """
-      Takes in a file with lines of 'route,costs' and builds a Trie of the routes' digits.
-    """
-    for line in open(file):
-      current = self.root
-      route, cost = line.split(",")
-      cost = float(cost.strip("\n"))
-      for digit in route:
-        if digit not in current.dictionary:
-            current.dictionary[digit] = TrieNode(digit)
-        current = current.dictionary[digit]
-      if current.cost:
-        if current.cost > cost:
-            current.cost = cost
-      else:
-        current.cost = cost
-
-  def findLowestCostsAndPrintThemToFile(self, file):
-    """Finds the lowest costs for each phone number in the input file."""
-
-    for phoneNumber in open(file):
-      current = self.root
-      minimum = float('inf')
-      for digit in phoneNumber:
-        if digit in current.dictionary:
-          if current.cost:
-              minimum = min(minimum, current.cost)
-          current = current.dictionary[digit]
-        else:
-          phoneNumber = phoneNumber.strip("\n")
-          if minimum != float('inf'):
-              with open("output_logs/route-costs-3.txt", "a+") as f:
-                      f.write(phoneNumber + ", " + str(minimum) + '\n')
-          else:
-              with open("output_logs/route-costs-3.txt", "a+") as f:
-                      f.write(phoneNumber + ", 0 \n")
-          break  # break out of the 'for digit in phoneNumber' loop
-          # and start on the next phoneNumber.
-
-# ideas: sorting our data:
-class Quick_Sort(object):
-  def __init__(self, arbitrary_order):
-    pass
-
-  def quick_sort(self, arbitrary_order):
-    self.quick_sort2(arbitrary_order, 0, len(arbitrary_order)-1)
-
-  def quick_sort2(self, arbitrary_order, low, high):
-    if low < high:
-      p = self.partition(arbitrary_order, low, high)
-      self.quick_sort2(arbitrary_order, low, p - 1)
-      self.quick_sort2(arbitrary_order, p + 1, high)
-
-  def get_pivot(self, arbitrary_order, low, high):
-    mid = (high + low) // 2
-    pivot = high
-    if arbitrary_order[low] < arbitrary_order[mid]:
-      if arbitrary_order[mid] < arbitrary_order[high]:
-        pivot = mid
-    elif arbitrary_order[low] < arbitrary_order[high]:
-      pivot = low
-    return pivot
-
-  def partition(self, arbitrary_order, low, high):
-    pivotIndex = self.get_pivot(arbitrary_order, low, high)
-    pivotValue = arbitrary_order[pivotIndex]
-    arbitrary_order[pivotIndex], arbitrary_order[low] = arbitrary_order[low], arbitrary_order[pivotIndex]
-    border = low
-
-    for i in range(low, high + 1):
-      if arbitrary_order[i] < pivotValue:
-        border += 1
-        arbitrary_order[i], arbitrary_order[border] = arbitrary_order[border], arbitrary_order[i]
-    arbitrary_order[low], arbitrary_order[border] = arbitrary_order[border], arbitrary_order[low]
-    return border
 
 
-if __name__ == "__main__":
-  pass
+
+
+
+# def buildTrieOfRoutes(self, file):
+#   """
+#     Takes in a file with lines of 'route,costs' and builds a Trie of the routes' digits.
+#   """
+#   for line in open(file):
+#     current = self.root
+#     route, cost = line.split(",")
+#     cost = float(cost.strip("\n"))
+#     for digit in route:
+#       if digit not in current.dictionary:
+#           current.dictionary[digit] = Trie(digit)
+#       current = current.dictionary[digit]
+#     if current.cost:
+#       if current.cost > cost:
+#           current.cost = cost
+#   else:
+#     current.cost = cost
+
+#   def findLowestCostsAndPrintThemToFile(self, file):
+#     """Finds the lowest costs for each phone number in the input file."""
+
+#     for phoneNumber in open(file):
+#       current = self.root
+#       minimum = float('inf')
+#       for digit in phoneNumber:
+#         if digit in current.dictionary:
+#           if current.cost:
+#               minimum = min(minimum, current.cost)
+#           current = current.dictionary[digit]
+#         else:
+#           phoneNumber = phoneNumber.strip("\n")
+#           if minimum != float('inf'):
+#               with open("output_logs/route-costs-3.txt", "a+") as f:
+#                       f.write(phoneNumber + ", " + str(minimum) + '\n')
+#           else:
+#               with open("output_logs/route-costs-3.txt", "a+") as f:
+#                       f.write(phoneNumber + ", 0 \n")
+#           break  # break out of the 'for digit in phoneNumber' loop
+#           # and start on the next phoneNumber.
